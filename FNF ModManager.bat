8539f0ddeaaff7acc70a2eb5f8122a753927fd92
YumDev12 <91219991+YumDev12@users.noreply.github.com>
2021-10-01 14:27:53 -0700

    More Mods

--- FNF ModManager.bat
+++ FNF ModManager.bat
@@ -9,8 +9,10 @@ echo Welcome To The Mod Manager For FNF
 echo edit the batch file
 echo Do This Code 
 echo 1. examplemod
-choice /c 1 
+echo 2. VsByteWolf
+choice /c 12 
 if %errorlevel% == 1 goto examplemod
+if %errorlevel% == 2 goto bytewolf
 
 :examplemod
 cls
@@ -18,3 +20,11 @@ title examplemod
 cd C:\Users\Riley\Documents\GitHub\FnF-Mod-Manager\mod
 cd C:\Users\Riley\Documents\GitHub\FnF-Mod-Manager\mod\examplemod
 start Funkin.exe
+goto mainmenustate
+
+:bytewolf
+cls
+title Bytewolf
+cd C:\Users\Riley\Documents\GitHub\FnF-Mod-Manager\mod
+cd C:\Users\Riley\Documents\GitHub\FnF-Mod-Manager\mod\Bytewolf
+start VSByteWolf.exe
